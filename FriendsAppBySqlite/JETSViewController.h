4f5a0582749e2b3b7a3fcd57c2b78c2d97cf7fdc
JETS <participant@Ahmeds-Mac.local>
2018-02-26 11:05:28 +0200

    Initial Commit

---
+++ FriendsAppBySqlite/JETSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JETSViewController.h
+//  FriendsAppBySqlite
+//
+//  Created by JETS on 2/26/18.
+//  Copyright (c) 2018 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JETSViewController : UIViewController
+
+@end
---
+++ FriendsAppBySqlite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FriendsAppBySqlite
+//
+//  Created by JETS on 2/26/18.
+//  Copyright (c) 2018 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JETSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JETSAppDelegate class]));
+    }
+}
